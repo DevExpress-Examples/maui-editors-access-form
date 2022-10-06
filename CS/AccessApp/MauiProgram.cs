4f149669f8b33097a92ff4bcc237d99ce03ad8d4
Anthony Sam <anton.sameishchev@devexpress.com>
2022-10-06 17:53:09 +0400

    Update MauiProgram.cs

--- CS/AccessApp/MauiProgram.cs
+++ CS/AccessApp/MauiProgram.cs
@@ -3,21 +3,17 @@ using DevExpress.Maui.Editors;
 
 namespace AccessApp;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
+public static class MauiProgram {
+	public static MauiApp CreateMauiApp() {
 		var builder = MauiApp.CreateBuilder();
 		builder
-			.UseMauiApp<App>()
-            .UseDevExpress()
-            .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-		return builder.Build();
+		.UseMauiApp<App>()
+        	.UseDevExpress()
+         	.ConfigureFonts(fonts =>
+		{
+			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+		});
+	return builder.Build();
 	}
 }
-
